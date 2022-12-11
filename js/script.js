955fa61b43c4dfb533d8f79f19aeef556f811295
Apenas Esdras <93005998+ApenasEsdras@users.noreply.github.com>
2022-12-11 20:28:28 -0300

    aat

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
-
 const sr = ScrollReveal ({
-    distance: `40px`,
-    duration: 2600,
-    reset: true
+  distance: `40px`,
+  duration: 2600,
+  reset: true
 })
 
 sr.reveal(`.text`,{delay:300, origin: `left` })
 
-// SocialLinks
-const LinksSocialMedia = {
-    github: 'ApenasEsdras',
-    youtube: 'UCrnSDklw5W1jTxv7zT2h6DQ',
-    facebook: 'ApenasEsdrasOfc',
-    instagram: 'apenasesdras',
-    twitter: 'apenasEsdras'
-  }
+// // SocialLinks
+// const LinksSocialMedia = {
+//     github: 'ApenasEsdras',
+//     // youtube: 'UCrnSDklw5W1jTxv7zT2h6DQ',
+//     facebook: 'ApenasEsdrasOfc',
+//     instagram: 'apenasesdras',
+//     twitter: 'apenasEsdras'
+//   }
 
-  function changeSocialMediaLinks() {
-    for (let li of socialLinks.children) {
-      const social = li.getAttribute('class')
+//   function changeSocialMediaLinks() {
+//     for (let li of socialLinks.children) {
+//       const social = li.getAttribute('class')
 
-      li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+//       li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
 
-    }
-  }
+//     }
+//   }
 
 //   Sobre min
